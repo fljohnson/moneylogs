5b2b339ba3c2586d9d96902303c0594774456d87
Floyd Johnson <fljohnson3@isp.com>
2018-10-16 14:21:16 -0400

    Filter by date; attempts at visual touchups

--- MoneyLogs/LoggingViewController.swift
+++ MoneyLogs/LoggingViewController.swift
@@ -255,9 +255,13 @@ override func viewWillAppear(_ animated: Bool)
 	//mangle it
 	
 	//set up the date strings for querying - ISO8601 style
+	/*
 	let options: ISO8601DateFormatter.Options = [.withFullDate, .withDashSeparatorInDate]
 	dbDates[0] = ISO8601DateFormatter.string(from: monthstart, timeZone: TimeZone.current, formatOptions: options)
 	dbDates[1] = ISO8601DateFormatter.string(from: monthend, timeZone: TimeZone.current, formatOptions: options)
+	*/
+	dbDates[0] = "2018-09-01"
+	dbDates[1] = "2018-09-30"
 	
 	//now for the UI
 	
